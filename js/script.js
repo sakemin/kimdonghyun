018ac54f52677f84426a58397edd035c22ae88f4
종민 정 <Uh@Uh-MacAir.local>
2020-01-14 18:29:12 +0900

    bottom menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ jQuery.noConflict();
 jQuery(document).ready(function(){
     var $ = jQuery;
 
+
     var scrolling;
 
 
@@ -14,12 +15,20 @@ jQuery(document).ready(function(){
     $(window).scroll(function(){
       var scrollHeight = $(document).height() - $(window).height();
       var nowHeight = $(window).scrollTop();
-      $(".pro-bar").width(nowHeight / scrollHeight * $(window).width());
+      $(".pro_bar").width(nowHeight / scrollHeight * $(window).width());
       clearInterval(scrolling);
+
+      var height = $(document).scrollTop(); //실시간으로 스크롤의 높이를 측정
+      if(height > 167){
+        $('.btm_menu').addClass('fixfix');
+        console.log("!");
+      }else if(height <= 167){
+        $('.btm_menu').removeClass('fixfix');
+      }
     });
 
     if ($(document).height() == $(window).height()) {
-      $(".pro-bar").css("width", "100%");
+      $(".pro_bar").css("width", "100%");
     }
 
     // $.fn.scrollEnd = function(callback, timeout) {
